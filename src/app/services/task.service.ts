91e8949628d639413cce2ea9579ce3c9bdcef90c
Simon Miller <mail@simonmiller.se>
2022-03-16 22:16:52 +0100

    add addTask form

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -13,7 +13,7 @@ const httpOptions = {
   providedIn: 'root'
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:3000/tasks'
+  private apiUrl = 'http://localhost:5000/tasks'
 
   constructor(private http:HttpClient) { }
 
